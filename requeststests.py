621f2a8d40ffce9fde113b08126bca089bd04ba4
Jesus Leyva <jleyva82@gmail.com>
2019-01-31 22:07:25 -0800

    Add files via upload

--- requeststests.py
+++ requeststests.py
@@ -6,16 +6,15 @@ Purpose:
     Exposure to HTML codes
     creation of HTML text file off an existing web address
 '''
-import requests                                  #import requests library
+import requests                                                         #import requests library
 
 
-params = {"q": "pizza"}
-r = requests.get("http://www.bing.com/search", params=params)            #scrapes the information of google.com as a python value
-print("Web Address:", r.url)
-print("Status:", r.status_code)                  #print the sites status code
-                                                 #should be code: 200, meaning all is well
-#print(r.text)
+params = {"q": "pizza"}                                                 #our example quiery will be for pizza
+r = requests.get("http://www.bing.com/search", params=params)           #scrapes the information of bing.com, search pizza, as a python value
+print("Web Address:", r.url)                                            #show the webaddress that was scrapped
+print("Status:", r.status_code)                                         #print the sites status code
+                                                                        #should be code: 200, meaning all is well
 
-f = open("./page.html", "w+")                    #creates page.html file in writable mode as python varianble
-f.write(r.text)                                  #in new file write the text data collected of google.com
+f = open("./page.html", "w+")                                           #creates page.html file in writable mode as python varianble
+f.write(r.text)                                                         #in new file write the text data collected of google.com
 
