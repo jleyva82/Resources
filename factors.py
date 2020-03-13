5f38c3c7503ad1a5b070c6c405723bac8bae2784
Jesus Leyva <jleyva82@gmail.com>
2020-03-12 18:03:55 -0700

    is it prime?

---
+++ factors.py
@@ -0,0 +1,19 @@
+#Author: Jesus Leyva
+#This returns a list of all factors given a number
+
+n = input("Give me a whole number:")
+n = int(n)
+
+factors = []
+factor_count = 1
+for x in range(1,n):
+ if n % x == 0:
+ 	factors.append(x)
+ 	factor_count += 1
+ 
+factors.append(n)
+print(factors)
+print("The number", n, "has", factor_count, "factors")
+
+if len(factors) == 2:
+	print("BINGO! This is a prime number.")
